a3070e8eb1806dc24f906f8c0a7001132fee5842
kyorohiro <kyorohiro@gmail.com>
2019-12-22 03:04:00 +0900

    ..

---
+++ w/dart_cli/app/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:app/app.dart' as app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${app.calculate()}!');
+}
---
+++ w/dart_cli/app/test/app_test.dart
@@ -0,0 +1,8 @@
+import 'package:app/app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
